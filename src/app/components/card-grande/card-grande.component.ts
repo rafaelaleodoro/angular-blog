7cf6b099210c142c9275f40cf86e084f367378c7
Rafaela Leodoro <108367324+rafaelaleodoro@users.noreply.github.com>
2023-02-06 19:31:00 -0300

    propriedades dinâmicas dos componentes

--- src/app/components/card-grande/card-grande.component.ts
+++ src/app/components/card-grande/card-grande.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-grande',
   templateUrl: './card-grande.component.html',
-  styleUrls: ['./card-grande.component.css']
+  styleUrls: ['./card-grande.component.css'],
 })
 export class CardGrandeComponent implements OnInit {
+  @Input()
+  fotoCover: string = "";
+  @Input()
+  cardTitulo: string = '';
+  @Input()
+  cardDescricao: string = "";
+  
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/menu-titulo/menu-titulo.component.ts
+++ src/app/components/menu-titulo/menu-titulo.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu-titulo',
   templateUrl: './menu-titulo.component.html',
-  styleUrls: ['./menu-titulo.component.css']
+  styleUrls: [
+    './menu-titulo.component.css',
+    './menu-titulo.responsivo.component.css',
+  ],
 })
 export class MenuTituloComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
